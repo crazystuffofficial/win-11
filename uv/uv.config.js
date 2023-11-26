3d1bc33cd2cd7a34114e9b16ed009fe3248fd1d7
Crazystuffxyz <125907466+crazystuffofficial@users.noreply.github.com>
2023-11-26 12:01:43 -0500

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,7 +1,7 @@
 /*global Ultraviolet*/
 self.__uv$config = {
     prefix: '/win-11/uv/service/',
-    bare: 'https://3c54nh-8080.csb.app/bare/',
+    bare: atob(localStorage.getItem("bare")),
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/win-11/uv/uv.handler.js',
